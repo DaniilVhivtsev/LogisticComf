ecbe5c927deebe74b5640a5480594c2beb62bf27
Daniil <danya.vshivtsev@mail.ru>
2021-10-14 23:20:54 +0500

    Make registration, login and roles

--- src/main/java/com/logisticcomfort/configuration/WebSecurityConfig.java
+++ src/main/java/com/logisticcomfort/configuration/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import com.logisticcomfort.model.Role;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,6 +14,7 @@ import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true )
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private DataSource dataSource;
@@ -26,8 +28,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/registration").permitAll()
-                .antMatchers("/").hasRole("HR")
+                .antMatchers("/", "/registration").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
@@ -43,8 +44,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         auth.jdbcAuthentication()
                 .dataSource(dataSource)
                 .passwordEncoder(NoOpPasswordEncoder.getInstance())
-                .usersByUsernameQuery("select username, password, enabled from users where username=?");
-//                .usersByUsernameQuery("select username, password, active from usr where username=?")
-//                .authoritiesByUsernameQuery("select u.username, ur.roles from usr u inner join user_role ur on u.id = ur.user_id where u.username=?");
+                .usersByUsernameQuery("select username, password, active from usr where username=?")
+                .authoritiesByUsernameQuery("select u.username, ur.roles from usr u inner join user_role ur on u.id = ur.user_id where u.username=?");
     }
 }
--- src/main/java/com/logisticcomfort/controllers/registryController.java
+++ src/main/java/com/logisticcomfort/controllers/registryController.java
@@ -1,60 +1,35 @@
 package com.logisticcomfort.controllers;
 
-import com.logisticcomfort.DAO.PersonDAO;
-import com.logisticcomfort.model.Person;
 import com.logisticcomfort.model.Role;
 import com.logisticcomfort.model.User;
 import com.logisticcomfort.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.validation.Valid;
 import java.util.Collections;
 
 @Controller
 public class registryController {
-
-    private final PersonDAO personDAO;
-
-    @Autowired
-    public registryController(PersonDAO personDAO) {
-        this.personDAO = personDAO;
-    }
 
     @Autowired
     private UserRepo userRepo;
 
     @GetMapping("/registration")
     public String registration(Model model) {
-        model.addAttribute("person", new Person());
         return "registration";
     }
 
     @PostMapping("/registration")
-    public String addUser(@ModelAttribute("person") @Valid Person person,
-                          BindingResult bindingResult){
-//        User userFromDb =  userRepo.findByUsername(user.getUsername());
-//
-////        if(userFromDb != null){
-////            //если такой пользователь есть
-////        }
-//
-//        user.setActive(true);
-//        user.setRoles(Collections.singleton(Role.USER));
-//        userRepo.save(user);
-//        return "redirect:/login";
+    public String addUser(User user){
+        User userFromDb =  userRepo.findByUsername(user.getUsername());
 
-//        if(bindingResult.hasErrors())
-//            return "/registration";
-
-        personDAO.Save(person);
-        return "login";
-
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        userRepo.save(user);
+        return "redirect:/login";
     }
 
     @GetMapping()
